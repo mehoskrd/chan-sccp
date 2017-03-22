2d809e0d128d51abab33acb470f04745754ee62f
Diederik de Groot <dkgroot@talon.nl>
2017-03-22 23:40:17 +0100

    Fix: Re-enable sccp_handle_port_reponse to set the report port early

--- src/sccp_actions.c
+++ src/sccp_actions.c
@@ -3314,12 +3314,9 @@ void handle_port_response(constSessionPtr s, devicePtr d, constMessagePtr msg_in
 				return;
 		}
 		
-		/*
 		if (channel && !sccp_netsock_equals(&sas, &rtp->phone_remote)) {
 			sccp_rtp_set_phone(channel, rtp, &sas);
-			//rtp->receiveChannelState = SCCP_RTP_STATUS_PORTSET;
 		}
-		*/
 	}
 }
 
